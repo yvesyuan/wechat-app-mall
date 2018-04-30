3fc50caa1cb2e6befd15a39a3997661900592dcf
Yuan Yong <yves.yuan@gmail.com>
2018-04-30 17:32:45 +0800

    Update for SLOWSPACE

--- pages/my/index.js
+++ pages/my/index.js
@@ -36,7 +36,7 @@ Page({
   aboutUs : function () {
     wx.showModal({
       title: '关于我们',
-      content: '本系统基于开源小程序商城系统 https://github.com/EastWorld/wechat-app-mall 搭建，祝大家使用愉快！',
+      content: '本系统为慢时空烘焙官方自助下单小程序，祝大家使用愉快！',
       showCancel:false
     })
   },
